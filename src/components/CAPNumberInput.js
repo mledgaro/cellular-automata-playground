751047ca0f0e8f2b8d528224472f4b896d223e8d
Edgar Mendoza <mledgaro@gmail.com>
2023-02-14 16:04:45 -0600

    Custom number input

--- src/components/CAPNumberInput.js
+++ src/components/CAPNumberInput.js
@@ -1,19 +1,37 @@
 //
 
-function CAPNumberInput(props) {
-    return (
-        <div class="input-group input-group-lg d-flex justify-content-center">
-            <span class="input-group-text cap-text-label">{props.label}</span>
+import CAPButton from "./CAPButton";
 
-            <input
-                type="number"
-                class="form-control rounded-end cap-input"
-                value={props.value}
-                min={props.min}
-                max={props.max}
-                step="1"
-                style={{maxWidth: "5rem"}}
-            />
+function CAPNumberInput(props) {
+    //
+
+    let decreaseValue = () => {
+        if (props.value > props.min) {
+            props.setValue(props.value - 1);
+        }
+    };
+
+    let increaseValue = () => {
+        if (props.value < props.max) {
+            props.setValue(props.value + 1);
+        }
+    };
+
+    return (
+        <div className="input-group d-flex justify-content-end">
+            {props.label != null && (
+                <span className="input-group-text cap-text-label">
+                    {props.label}
+                </span>
+            )}
+
+            <CAPButton iconId="minus" onClick={decreaseValue} />
+
+            <span className="input-group-text cap-text-label">
+                {props.value}
+            </span>
+
+            <CAPButton iconId="plus" onClick={increaseValue} />
         </div>
     );
 }
