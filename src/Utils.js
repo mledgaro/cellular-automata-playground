0d6b8f0a19b56a8bb3be6139466336b22c60b168
Edgar Mendoza <mledgaro@gmail.com>
2023-02-15 09:47:51 -0600

    Utils file for general purpose functions

---
+++ src/Utils.js
@@ -0,0 +1,41 @@
+//
+
+/**
+ * Converts an integer number to an array of booleans that represents
+ * the digits of its binary representation.
+ * E.G.
+ * 30 -> 00011110_b -> [false, true, true, true, true, false, false, false]
+ * @param {int} int integer number
+ * @param {int} size array size
+ * @returns {Array} array of booleans
+ */
+export function intToBoolArray(int, size) {
+    let arr;
+
+    arr = "0".repeat(size) + int.toString(2);
+    arr = arr.slice(-size);
+    arr = arr.split("");
+    arr = arr.map((e) => e == "1");
+    arr = arr.reverse();
+
+    return arr;
+}
+
+/**
+ * Converts a boolean array to an integer number whose binary digits are
+ * the booleans of the array.
+ * E.G.
+ * [false, true, true, true, true, false, false, false] -> 00011110_b -> 30
+ * @param {Array} arr boolean array
+ * @returns {int} integer number
+ */
+export function boolArrayToInt(arr, reverse) {
+    let int;
+
+    int = reverse ? arr.reverse() : arr;
+    int = int.map((e) => (e ? "1" : "0"));
+    int = int.join("");
+    int = parseInt(int, 2);
+
+    return int;
+}
