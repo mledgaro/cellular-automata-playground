e53a0a447589006861ef85d269791293ac0e33e8
Edgar Mendoza <mledgaro@gmail.com>
2023-02-21 15:00:25 -0600

    Allow to directly edit the number in input number

--- src/components/NumberInput.jsx
+++ src/components/NumberInput.jsx
@@ -1,5 +1,6 @@
 //
 
+import { useState } from "react";
 import { inputGroupClasses } from "../js/Utils";
 import Button from "./Button";
 
@@ -14,6 +15,8 @@ export default function NumberInput({
 }) {
     //
 
+    let [tmp, setTmp] = useState(value.get);
+
     return (
         <div className={inputGroupClasses(size, alignment, ` ${extraClasses}`)}>
             {label != null && (
@@ -25,17 +28,38 @@ export default function NumberInput({
             <Button
                 icon={{ id: "minus" }}
                 enabled={value.get > min}
-                onClick={() => value.set(value.get - 1)}
+                onClick={() => {
+                    value.set(value.get - 1);
+                    setTmp(tmp - 1);
+                }}
             />
 
-            <span className="input-group-text cap-container-dark-1">
-                {value.get}
-            </span>
+            <input
+                type="text"
+                className=" cap-container-dark-1 cap-input-text"
+                value={tmp}
+                onChange={(e) => setTmp(e.target.value)}
+                onBlur={(e) => {
+                    if (!isNaN(tmp)) {
+                        let val = Number(tmp);
+                        val = val < min ? min : val > max ? max : val;
+                        setTmp(val);
+                        value.set(val);
+                    } else {
+                        setTmp(min);
+                        value.set(min);
+                    }
+                }}
+                size={1}
+            />
 
             <Button
                 icon={{ id: "plus" }}
                 enabled={value.get < max}
-                onClick={() => value.set(value.get + 1)}
+                onClick={() => {
+                    value.set(value.get + 1);
+                    setTmp(tmp + 1);
+                }}
             />
         </div>
     );
