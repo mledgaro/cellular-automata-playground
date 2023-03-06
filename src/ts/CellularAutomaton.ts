5ff5289854974e522fb7eb0e16572285188f4bb0
Edgar Mendoza <mledgaro@gmail.com>
2023-03-06 08:32:53 -0600

    Complete 1d model

--- src/ts/CellularAutomaton.ts
+++ src/ts/CellularAutomaton.ts
@@ -1,98 +1,32 @@
 //
 
-import { boolArrayToInt, randomBoolArray } from "./Utils";
+import { boolArrayToInt } from "./Utils";
+
+export type NbhdType = "adjacent" | "grouped" | "scattered";
 
 export default class CellularAutomaton {
     //
 
-    #MAX_CELLS = 512;
+    #numCells: number;
+    #cellsNbhd: number[][]; // Array(Array(Integer))[cellsNumber][nbhdWidth]
+    #rules: boolean[];
+    #state: boolean[];
 
-    #cellsNumber: number = 256; // Integer
-    #nbhdWidth: number = 3; // Integer
-    #cellsNbhd: number[][] = CellularAutomaton.cellsNbhds(
-        "adjacent",
-        this.#cellsNumber,
-        this.#nbhdWidth,
-        1
-    ); // Array(Array(Integer))[cellsNumber][nbhdWidth]
-    #numRules: number = Math.pow(2, this.#nbhdWidth); // Int = 2^nbhdWidth
-    #rules: boolean[] = randomBoolArray(this.#numRules); // Array(Bool)[numRules]
-    #state: boolean[] = randomBoolArray(this.#cellsNumber); // Array(Bool)[cellsNumber]
+    constructor() {
+        //
+        this.#numCells = 256;
+        this.#cellsNbhd = [];
+        this.#rules = [];
+        this.#state = Array(this.#numCells).fill(false);
 
-    // constructor() {
-    //     //
-    // }
+        this.setCellsNbhds(3, "adjacent", 1);
+    }
 
     get cellsNumber(): number {
-        return this.#cellsNumber;
+        return this.#numCells;
     }
 
-    set cellsNumber(number: number) {
-        this.#cellsNumber = Math.min(Math.max(number, 0), this.#MAX_CELLS);
-    }
-
-    get nbhdWidth(): number {
-        return this.#nbhdWidth;
-    }
-
-    set nbhdWidth(width: number) {
-        this.#nbhdWidth = width;
-        this.#numRules = Math.pow(2, width);
-    }
-
-    cellNbhd(index: number): number[] {
-        return this.#cellsNbhd[index];
-    }
-
-    setNbhds(type: NbhdType, mainCell: number) {}
-
-    get numRules(): number {
-        return this.#numRules;
-    }
-
-    ruleState(rule: number) {}
-
-    setRule(rule: number, state: boolean) {}
-
-    get state(): boolean[] {
-        return this.#state;
-    }
-
-    set state(state: boolean[]) {}
-
-    #cellNextState(index: number): boolean {
-        //
-
-        let nstate;
-
-        nstate = this.#cellsNbhd[index];
-        nstate = nstate.map((e) => this.state[e]);
-        nstate = boolArrayToInt(nstate, false);
-        nstate = this.#rules[nstate];
-
-        return nstate;
-    }
-
-    nextState(): boolean[] {
-        //
-
-        let nstate = this.state.map((cell, i, state_) =>
-            this.#cellNextState(i)
-        );
-
-        this.state = nstate;
-
-        return nstate;
-    }
-
-    // static functions
-
-    static adjacentNbhd(
-        index: number,
-        width: number,
-        main: number,
-        numCells: number
-    ): number[] {
+    #adjacentNbhd(index: number, width: number, main: number): number[] {
         //
 
         let nbhd = [];
@@ -104,26 +38,21 @@ export default class CellularAutomaton {
         }
 
         if (a < 0) {
-            nbhd = nbhd.map((e) => (e < 0 ? numCells + e : e));
+            nbhd = nbhd.map((e) => (e < 0 ? this.#numCells + e : e));
         }
 
-        if (b >= numCells) {
-            nbhd = nbhd.map((e) => e % numCells);
+        if (b >= this.#numCells) {
+            nbhd = nbhd.map((e) => e % this.#numCells);
         }
 
         return nbhd;
     }
 
-    static groupedNbhd(
-        index: number,
-        width: number,
-        main: number,
-        numCells: number
-    ): number[] {
+    #groupedNbhd(index: number, width: number, mainCell: number): number[] {
         //
 
         let nbhd = [];
-        let a = Math.round(Math.random() + numCells);
+        let a = Math.round(Math.random() + this.#numCells);
         let b = a + width;
 
         for (let i = a; i < b; i++) {
@@ -131,92 +60,253 @@ export default class CellularAutomaton {
         }
 
         if (a < 0) {
-            nbhd = nbhd.map((e) => (e < 0 ? numCells + e : e));
+            nbhd = nbhd.map((e) => (e < 0 ? this.#numCells + e : e));
         }
 
-        if (b >= numCells) {
-            nbhd = nbhd.map((e) => e % numCells);
+        if (b >= this.#numCells) {
+            nbhd = nbhd.map((e) => e % this.#numCells);
         }
 
-        if (main >= 0) {
-            nbhd.splice(main, 1, index);
+        if (mainCell >= 0) {
+            nbhd.splice(mainCell, 1, index);
         }
 
         return nbhd;
     }
 
-    static scatteredNbhd(
-        index: number,
-        width: number,
-        main: number,
-        numCells: number
-    ): number[] {
+    #scatteredNbhd(index: number, width: number, mainCell: number): number[] {
         //
 
         let nbhd = [];
 
         for (let i = 0; i < width; i++) {
-            nbhd.push(Math.round(Math.random() + numCells));
+            nbhd.push(Math.round(Math.random() + this.#numCells));
         }
 
-        if (main >= 0) {
-            nbhd.splice(main, 1, index);
+        if (mainCell >= 0) {
+            nbhd.splice(mainCell, 1, index);
         }
 
         return nbhd;
     }
 
-    static cellsNbhds(
-        type: NbhdType,
-        numCells: number,
-        nbhdWidth: number,
-        mainCell: number
-    ): number[][] {
+    setCellsNbhds(width: number, type: NbhdType, mainCell: number) {
         //
 
         let nbhds = [];
+        let getNbhd;
 
         switch (type) {
             case "adjacent":
-                for (let i = 0; i < numCells; i++) {
-                    nbhds.push(
-                        CellularAutomaton.adjacentNbhd(
-                            i,
-                            nbhdWidth,
-                            mainCell,
-                            numCells
-                        )
-                    );
-                }
+                getNbhd = (index: number) =>
+                    this.#adjacentNbhd(index, width, mainCell);
                 break;
             case "grouped":
-                for (let i = 0; i < numCells; i++) {
-                    nbhds.push(
-                        CellularAutomaton.groupedNbhd(
-                            i,
-                            nbhdWidth,
-                            mainCell,
-                            numCells
-                        )
-                    );
-                }
+                getNbhd = (index: number) =>
+                    this.#groupedNbhd(index, width, mainCell);
                 break;
             case "scattered":
-                for (let i = 0; i < numCells; i++) {
-                    nbhds.push(
-                        CellularAutomaton.scatteredNbhd(
-                            i,
-                            nbhdWidth,
-                            mainCell,
-                            numCells
-                        )
-                    );
-                }
+                getNbhd = (index: number) =>
+                    this.#scatteredNbhd(index, width, mainCell);
                 break;
         }
 
-        return nbhds;
+        for (let i = 0; i < this.#numCells; i++) {
+            nbhds.push(getNbhd(i));
+        }
+
+        this.#cellsNbhd = nbhds;
+        this.#rules = Array(Math.pow(2, width)).fill(false);
+    }
+
+    getCellNbhd(index: number): number[] {
+        return this.#cellsNbhd[index];
+    }
+
+    setRule(index: number, state: boolean) {
+        //
+
+        this.#rules[index] = state;
+    }
+
+    toggleRule(index: number) {
+        this.#rules[index] = !this.#rules[index];
+    }
+
+    setRandomRules() {
+        //
+
+        for (let i = 0; i < this.#rules.length; i++) {
+            this.#rules[i] = Math.random() <= 0.5;
+        }
+    }
+
+    setRulesAlive() {
+        //
+
+        for (let i = 0; i < this.#rules.length; i++) {
+            this.#rules[i] = true;
+        }
+    }
+
+    setRulesDead() {
+        //
+
+        for (let i = 0; i < this.#rules.length; i++) {
+            this.#rules[i] = false;
+        }
+    }
+
+    getRule(index: number): boolean {
+        return this.#rules[index];
+    }
+
+    /**
+     * Returns an array of integers between 'minVal' and 'maxVal' whose sum equals totalSum.
+     * @param {Int} minVal min value
+     * @param {Int} maxVal max value
+     * @param {Int} totalSum total sum
+     */
+    #variableDist(minVal: number, maxVal: number, totalSum: number): number[] {
+        //
+
+        let arr = [];
+        let diff = Math.abs(maxVal - minVal);
+        let sum = 0;
+        let num;
+
+        while (sum < totalSum) {
+            num = minVal + Math.round(Math.random() * diff);
+            arr.push(num);
+            sum += num;
+        }
+
+        arr[arr.length - 1] -= sum - totalSum;
+
+        return arr;
+    }
+
+    /**
+     * Returns an array of n = 'nums' random integers whose sum equals 'totalSum'.
+     * @param {Int} nums number of integers
+     * @param {Int} totalSum total sum
+     */
+    #randomDist(nums: number, totalSum: number): number[] {
+        //
+
+        let arr = [];
+        let sum = 0;
+
+        for (let i = 0, n; i < nums; i++) {
+            n = Math.round(Math.random() * totalSum);
+            arr.push(n);
+            sum += n;
+        }
+
+        let trim = Math.round((sum - totalSum) / nums);
+
+        for (let i = 0, diff, trimAc = 0; i < nums; i++) {
+            diff = arr[i] - (trim + trimAc);
+            if (diff > 0) {
+                arr[i] = diff;
+                trimAc = 0;
+            } else {
+                arr[i] = 1;
+                trimAc = Math.abs(diff + 1);
+            }
+        }
+
+        return arr;
+    }
+
+    /**
+     *
+     * @param liveCells Mumber of live cells. If it is smaller than one, it will be considered as percentage.
+     * @param groupMinSize
+     * @param groupMaxSize
+     * @param distributionType
+     * @returns
+     */
+    setState(
+        liveCells: number,
+        groupMinSize: number,
+        groupMaxSize: number,
+        distributionType: "rand" | "even"
+    ) {
+        //
+
+        if (liveCells >= 0 && liveCells < 1) {
+            liveCells = Math.round(liveCells * this.#numCells);
+        } else {
+            liveCells = Math.max(1, Math.min(liveCells, this.#numCells));
+        }
+
+        let deadCells = this.#numCells - liveCells;
+
+        let groupSizeDiff = Math.abs(groupMaxSize - groupMinSize);
+
+        let trueArr, falseArr;
+
+        if (groupSizeDiff > 0) {
+            trueArr = this.#variableDist(groupMinSize, groupMaxSize, liveCells);
+        } else {
+            trueArr = Array(Math.round(liveCells / groupMinSize)).fill(
+                groupMinSize
+            );
+        }
+
+        if (distributionType === "rand") {
+            falseArr = this.#randomDist(trueArr.length + 1, deadCells);
+        } else {
+            falseArr = Array(trueArr.length + 1).fill(
+                Math.round(deadCells / (trueArr.length + 1))
+            );
+        }
+
+        let arr: boolean[] = [];
+
+        for (let i = 0; i < trueArr.length; i++) {
+            arr = arr.concat(
+                Array(falseArr[i])
+                    .fill(false)
+                    .concat(Array(trueArr[i]).fill(true))
+            );
+        }
+
+        let diff = this.#numCells - arr.length;
+        if (diff <= 0) {
+            arr.slice(0, this.#numCells);
+        } else {
+            arr.concat(Array(diff).fill(false));
+        }
+
+        this.#state = arr;
+    }
+
+    get state(): boolean[] {
+        return this.#state;
+    }
+
+    #cellNextState(index: number): boolean {
+        //
+
+        let nstate;
+
+        nstate = this.#cellsNbhd[index];
+        nstate = nstate.map((e) => this.#state[e]);
+        nstate = boolArrayToInt(nstate, false);
+        nstate = this.#rules[nstate];
+
+        return nstate;
+    }
+
+    nextState(): boolean[] {
+        //
+
+        let nstate = this.#state.map((cell, i) => this.#cellNextState(i));
+
+        this.#state = nstate;
+
+        return nstate;
     }
 }
-
-export type NbhdType = "adjacent" | "grouped" | "scattered";
