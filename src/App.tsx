c91c5c3c893af48a0298c7c94f5e82810be4e603
Edgar Mendoza <mledgaro@gmail.com>
2023-08-31 18:22:56 -0600

    Select automata from title

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./css/App.css";
 
 import React, { useEffect, useRef } from "react";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import {
     StateObjHook,
     useAppDispatch,
@@ -11,7 +11,15 @@ import {
     useStateObj,
 } from "./app/hooks";
 
-import { Box } from "@mui/material";
+import {
+    Box,
+    Button,
+    Menu,
+    MenuItem,
+    MenuProps,
+    alpha,
+    styled,
+} from "@mui/material";
 import { selectNumCells } from "./app/slices/numCells";
 import CustomTabs from "./components/Tabs";
 import Nbhd1d from "./features/ca1d/Nbhd1d";
@@ -33,12 +41,17 @@ import { selectNbhdWidth } from "./app/slices/nbhdWidth";
 import { selectNbhdType } from "./app/slices/nbhdType";
 import { selectMainCell } from "./app/slices/mainCell";
 import { CellularAutomaton2d } from "./ts/CellularAutomaton2d";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function App() {
     //
     return (
         <BrowserRouter>
             <Box>
+                <Box className="mx-auto mt-4 w-fit">
+                    <CustomizedMenus />
+                </Box>
                 <Routes>
                     <Route index element={<CellularAutomata2d />} />
                     <Route path="ca1d" element={<CellularAutomata1d />} />
@@ -46,6 +59,87 @@ export default function App() {
                 </Routes>
             </Box>
         </BrowserRouter>
+    );
+}
+
+const StyledMenu = styled((props: MenuProps) => (
+    <Menu
+        elevation={0}
+        anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+        }}
+        transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+        }}
+        {...props}
+    />
+))(({ theme }) => ({
+    "& .MuiPaper-root": {
+        borderRadius: 6,
+        marginTop: 10,
+        minWidth: 180,
+        "& .MuiMenu-list": {
+            padding: "8px",
+            a: {
+                color: "var(--sunglow)",
+            },
+        },
+        backgroundColor: "var(--jet)",
+    },
+}));
+
+function CustomizedMenus() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const title = useStateObj<string>("1D Cellular Automata");
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    return (
+        <div>
+            <Button
+                id="demo-customized-button"
+                aria-controls={open ? "demo-customized-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                variant="contained"
+                onClick={handleClick}
+                className="bg-jet text-sunglow text-4xl font-bold"
+            >
+                {title.get}
+                <FontAwesomeIcon className="ml-2" icon={faChevronDown} />
+            </Button>
+            <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                    "aria-labelledby": "demo-customized-button",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+            >
+                <MenuItem
+                    onClick={() => {
+                        title.set("1D Cellular Automata");
+                        setAnchorEl(null);
+                    }}
+                >
+                    <Link to="ca1d">1D Cellular Automata</Link>
+                </MenuItem>
+                <MenuItem
+                    onClick={() => {
+                        title.set("2D Cellular Automata");
+                        setAnchorEl(null);
+                    }}
+                >
+                    <Link to="ca2d">2D Cellular Automata</Link>
+                </MenuItem>
+                {/* <Divider sx={{ my: 0.5 }} /> */}
+            </StyledMenu>
+        </div>
     );
 }
 
@@ -95,7 +189,6 @@ function CellularAutomata1d() {
 
     return (
         <Box className="space-y-6 my-5">
-            <Box className="cap-title">1D Cellular Automata</Box>
             <Scene init={init} next={next} />
             <CustomTabs
                 tabs={[
@@ -147,7 +240,6 @@ function CellularAutomata2d() {
 
     return (
         <Box className="space-y-6 my-5">
-            <Box className="cap-title">2D Cellular Automata</Box>
             <Scene init={init} next={next} state={initState.get} />
             <CustomTabs
                 tabs={[
